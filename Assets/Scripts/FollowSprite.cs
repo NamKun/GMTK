bdfe8d794255bcf6469adc33e361ac7d9c83923c
Brendan McManus <brendan.mcmanus7@mail.dcu.ie>
2020-07-11 21:40:23 +0100

    add repo

---
+++ Assets/Scripts/FollowSprite.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class FollowSprite : MonoBehaviour
+{
+
+    public Transform target;
+
+    public float smoothSpeed = 0.125f;
+
+    void LateUpdate()
+    {
+
+        transform.position = new Vector3(target.position.x, target.position.y, transform.position.z);
+    }
+}
---
+++ Assets/Scripts/VictoryBox.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class VictoryBox : MonoBehaviour
+{
+
+    void OnCollisionEnter(Collision vic) {
+        if(vic.collider.tag == "Victory Square"){
+            SceneManager.LoadScene("Finish");
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
